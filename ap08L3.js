adc500b5c5b29536228b4d62a6dad947646b5882
G484962024 <248496@st.takushoku-u.ac.jp>
2025-11-19 15:54:32 +0900

    L3 changed

--- ap08L3.js
+++ ap08L3.js
@@ -55,7 +55,23 @@ export function init(scene, size, id, offset, texture) {
         const height = [2, 2, 7, 4, 5];
         const bldgH = height[type]*5;
         const geometry = new THREE.BoxGeometry(8, bldgH, 8);
-        const material = new THREE.MeshLambertMaterial({color: 0x808080});
+        // const material = new THREE.MeshLambertMaterial({color: 0x808080});
+        const material = new THREE.MeshLambertMaterial({map: texture});
+        const sideUvS = (type*2+1)/11;
+        const sideUvE = (type*2+2)/11;
+        const topUvS = (type*2+2)/11;
+        const topUvE = (type*2+3)/11;
+        const uvs = geometry.getAttribute("uv");
+        for(let i=0; i<48; i+=4){
+            if(i<16||i>22){
+                uvs.array[i] = sideUvS;
+                uvs.array[i+2] = sideUvE;
+            }
+            else{
+                uvs.array[i] = topUvS;
+                uvs.array[i+2] = topUvE;
+            }
+        }
         const bldg = new THREE.Mesh(
             geometry,
             material
@@ -63,7 +79,8 @@ export function init(scene, size, id, offset, texture) {
         bldg.position.set(x, bldgH, z);
         scene.add(bldg);
     }
-    makeBuilding(20, -30, 0);
+    makeBuilding(-15, 45, 0);
+    makeBuilding(-40, 65, 0);
 
     // コース(描画)
     // 制御点を補完して曲線を作る
